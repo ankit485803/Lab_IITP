e993fe9e9a9dd4491cba1ef0c52aef0e066f3f93
Ankit Kumar <ankit485803@gmail.com>
2025-03-30 20:23:26 +0530

    solve stdVisitngCard and CountryInfro give a lesson how to json work and behind working

---
+++ 208_WebAppDev/29thMarch/Q1_StdRecordViewer/jquery_Syntax.js
@@ -0,0 +1,24 @@
+
+
+//  JQuery is a library of JS
+
+
+
+
+$(document).ready(function(){
+
+    // jQuery methods go here...
+  
+  });
+
+
+
+
+  $(document).ready(function(){
+    $("button").click(function(){
+      $("p").hide();
+    });
+  });
+
+
+  
