5ee262b29b666e16fea25838e4151b62e6f133c7
Ankit Kumar <ankit485803@gmail.com>
2025-09-03 21:31:30 +0530

    start the third yeear Sem5 OS 303 lab on Linux OS

---
+++ 303_OS/setups/newMachineSetup.cpp
@@ -0,0 +1,14 @@
+
+
+
+
+/*
+
+Linux is more powerful than window as this is open source
+
+two types of interface: GUI (graph user interface), CLI (command line interface)
+
+very helpful when website sever side deployments karne mai
+
+
+*/
