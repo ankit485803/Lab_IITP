ee8dd55eef9db2505cb5725e8e7531994ad8e561
Ankit Kumar <ankit485803@gmail.com>
2025-09-05 00:09:52 +0530

    complete all the fundaments of commands

--- 303_OS/setups_VM/newMachineSetup.sh
+++ 303_OS/setups_VM/newMachineSetup.sh
@@ -11,4 +11,18 @@ two types of interface: GUI (graph user interface), CLI (command line interface)
 very helpful when website sever side deployments karne mai
 
 
+Q. confusion clear Window PS and Linux WSL
+
+Ye Microsoft ka multipurpose terminal app hai.
+Ek container jaisa hai jisme aap PowerShell, Command Prompt (cmd), Azure Cloud Shell, aur WSL shells (Ubuntu, Debian, etc.) sab chala sakte ho.
+Default Windows 11 me hota hai (Start menu me search: Windows Terminal).
+
+# Linux Terminal (inside WSL)
+
+Ye WSL me chal raha shell hai (jaise Ubuntu ka bash, zsh).
+Jab aap wsl ya Ubuntu open karte ho, tab directly Linux environment ka terminal milta hai.
+Yaha prompt kuch aisa hota hai:
+username@PCname:/mnt/c/Users/username$
+
+
 */
