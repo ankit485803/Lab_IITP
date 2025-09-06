3458addcc75ac2b47d6ec9bdac5eddc6c367bdb8
Ankit Kumar <ankit485803@gmail.com>
2025-09-06 12:12:52 +0530

    Lab 2 done: Linux & Git commands, with images

---
+++ 303_OS/practical/impGitCommand.sh
@@ -0,0 +1,32 @@
+
+
+
+6th Sep 2025 (Satursday)
+
+
+impGitCommand
+
+
+# BASIC
+1. git branch  (kis branch mai hai dekhtne ke liye)
+    git branch -b newBranchName   (create karnke ke liye)   and git branch -d branchName    (jisko delete karna hai)
+    git switch main    (another branch pe jane ke liye)    git checkout main
+
+
+
+# MERGING CODE
+2. git diff branch1name branch2name
+    git merge 
+and  create the PR on github which you FORK open source project like GSOC / BITS Pillani with Saurav ji
+
+    git pull origin main   (ese final dowload to PC)
+
+
+
+# RESOLVING MERGE CONFILIT (events, manually batana hota hai kisko karna changes)
+3. git reseat    (case1: jab sirf add kar dete hai)
+    git reseat commit hashcode   (case2: jab hm add and commit dono kar dete hai fir hashCode dena hoga git log se)
+
+
+
+4. 
---
+++ 303_OS/practical/ruleLab.sh
@@ -0,0 +1,29 @@
+
+
+
+
+i want to work on learn-os-linux branch then merge to main after completed each lab class IITP
+
+
+# Already on learn-os-linux
+# Do your edits...
+
+git add .
+git commit -m "Completed Lab 1: Intro to basic Linux commands"
+
+git switch main
+git merge learn-os-linux
+git push origin main     # Optional
+git switch learn-os-linux
+
+
+
+
+
+🧠 Optional Tips
+Tag your labs (optional but useful):
+To easily track versions per lab:
+git tag lab-3
+
+Or more descriptive:
+git tag lab-3-file-permissions
