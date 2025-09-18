0cc7a5573a90be1952a2b910b6d7df6bac2ac89e
Ankit Kumar <ankit485803@gmail.com>
2025-09-18 17:10:59 +0530

    learn file permission chmod calculator from CodeWithHarry

---
+++ 303_OS/Linux/CodeWithHarry/FilePermission.sh
@@ -0,0 +1,24 @@
+
+
+
+:'
+
+
+18th Sep 2025 (Wednesday)  Learn the file permission (read, write, execute -- owner, group, public) concepts from code with harry
+
+
+
+video link: https://youtu.be/_tCY-c-sPZc?si=HBLgmV-OvHI7snsU
+
+
+change mode calculate:  https://chmod-calculator.com/
+
+uses the binary conversion concepts here ye log bhai ANkit
+
+
+Regular user symbol dollar $ and Sudo user HASH  #
+
+
+'
+
+Sudo install vim
