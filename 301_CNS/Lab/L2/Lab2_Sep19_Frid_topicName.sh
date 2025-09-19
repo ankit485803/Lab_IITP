9839307ea9a494ee6069b203d919ec578ea9710d
Ankit Kumar <ankit485803@gmail.com>
2025-09-19 16:41:26 +0530

    PL Bera Sir start the CNS lab class and install the Gedit - GNOME Text Editor in Linux

---
+++ 301_CNS/Lab/L2/Lab2_Sep19_Frid_topicName.sh
@@ -0,0 +1,45 @@
+
+
+sanja@IITP:/mnt/c/Users/sanja/Desktop/Lab/PLB_Sir$ ls
+chat_client.c  chat_server.c  ft_client.c  ft_server.c
+sanja@IITP:/mnt/c/Users/sanja/Desktop/Lab/PLB_Sir$ gedit chat_server.c
+Command 'gedit' not found, but can be installed with:
+sudo snap install gedit  # version 48.1, or
+sudo apt  install gedit  # version 46.1-3
+See 'snap info gedit' for additional versions.
+
+
+
+
+after install 
+sanja@IITP:~$ gedit
+sanja@IITP:~$ gedit --version
+gedit - Version 46.2
+sanja@IITP:~$
+
+
+
+
+
+:'
+
+
+
+19th Sep 2025 (Friday)  Lab2
+
+
+
+Gedit is a free and easy-to-use text editor for Linux, especially on systems using the GNOME desktop environment (like Ubuntu).
+ Its good for both coding and general
+ writing because it highlights code syntax, has a simple design, and includes useful features such as a file browser, 
+ search/replace, and autosave. You can also add more functions with plugins. Note that Gedit was replaced as
+  the default GNOME text editor by a newer application called "GNOME Text Editor" starting with GNOME 42 in 2022. 
+  The provided information also mentions a video showing how to install Gedit on Kali Linux.
+
+
+
+
+'
+
+
+sudo apt  install gedit
