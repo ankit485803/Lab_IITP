cc0270e13a0093bc4ed04d71e4ee7bf2e2f60ee6
Ankit Kumar <ankit485803@gmail.com>
2025-09-21 15:37:33 +0530

    ready for next Lab3 CNS fridya 26th Sep on RountingTable and Fragmentation

---
+++ 301_CNS/Lab/L3/Lab3_Sep26_Frid_topicName.sh
@@ -0,0 +1,20 @@
+
+
+
+:'
+
+
+26th Sep 2025 (Friday) Lab3 and topic covered: Routing Protocols & Routing Table and Fragmentation in networkLayer
+
+
+Q1. from one node source, How the packet get routed to destination ?
+Q2. Intermidated Router
+Q3. How the packet get fragemented from one machine to another machine
+
+
+
+
+
+
+
+'
