7ccbad101bd81b54fa75404c40182e586d9ff25d
Ankit Kumar <ankit485803@gmail.com>
2025-09-23 19:22:51 +0530

    Add dockerTutorialBegineer setup for Docker learning and create a separate branch for Docker and tag this also

---
+++ 311_CloudComputing/dockerTutorialBegineer/index/prompt_To_GPT.sh
@@ -0,0 +1,56 @@
+
+
+
+:'
+
+i start learning the Docker completer in my college IITP cloud computing couserswork and creating the repo : C:\Users\sanja\Desktop\GitProj\Lab_IITP\311_CloudComputing\dockerTutorialBegineer\oneShot_ByApnaCollege.sh
+
+i am beginner and start first time this, as i faced the earlier codes sharing issues while working on project ResearchKart      and Dcployrncnl & 1-csting: Deploying the platform and ensuring performance. scalability. and 
+ using Hostinger. ( Deployed link: https://researchkart.in/
+
+
+
+
+Q2. Docker i am asking to you file extension name: So, when you create the file, you just name it Dockerfile (without .txt, .sh, or any other extension). 
+It should be in the root of your project directory (or wherever you are building the Docker image). No need to add an extension because Docker automatically recognizes it by name.
+
+
+
+Q3. repo setup for docker
+
+ok thanks
+
+I want to set up this subfolder in the same GitHub repo C:\Users\sanja\Desktop\GitProj\Lab_IITP\311_CloudComputing\dockerTutorialBegineer
+and also a separate branch for that, where I can push all the commits related to Docker learn exploring skills
+
+Note: Be alert - this should not merge with my other rest branch, and push all learning skills in this docker-journey-devops branch only. and lastly merge with the main branch
+
+This is below for your reference: my current working status
+PS C:\Users\sanja\Desktop\GitProj\Lab_IITP> git branch           
+* learn-os-linux
+  main
+PS C:\Users\sanja\Desktop\GitProj\Lab_IITP> git remote -v        
+origin  https://ankit485803@github.com/ankit485803/Lab_IITP.git (fetch)
+origin  https://ankit485803@github.com/ankit485803/Lab_IITP.git (push)
+PS C:\Users\sanja\Desktop\GitProj\Lab_IITP> git config user.email
+ankit485803@gmail.com
+PS C:\Users\sanja\Desktop\GitProj\Lab_IITP> git config user.name 
+Ankit Kumar
+PS C:\Users\sanja\Desktop\GitProj\Lab_IITP>  
+
+
+Ans: git checkout -b docker-journey-devops
+      git add 311_CloudComputing
+      
+
+
+
+
+Q4. 
+
+
+
+
+
+
+'
