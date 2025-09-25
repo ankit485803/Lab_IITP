ab9cb9c105cf39f89e00eae5f5b69c52eb9d380e
Ankit Kumar <ankit485803@gmail.com>
2025-09-25 18:08:16 +0530

    Pulled and ran my first Docker image (hello-world) — created container successfully

---
+++ 311_CloudComputing/dockerTutorialBegineer/checkDowload.sh
@@ -0,0 +1,45 @@
+
+
+
+:'
+
+25th Sep 2025 (Thursday)
+
+PS C:\Users\sanja> docker
+Usage:  docker [OPTIONS] COMMAND
+
+A self-sufficient runtime for containers
+
+Common Commands:
+  run         Create and run a new container from an image
+  exec        Execute a command in a running container
+  ps          List containers
+  build       Build an image from a Dockerfile
+  bake        Build from a file
+  pull        Download an image from a registry
+  push        Upload an image to a registry
+  images      List images
+  login       Authenticate to a registry
+  logout      Log out from a registry
+  search      Search Docker Hub for images
+  version     Show the Docker version information
+  info        Display system-wide information
+
+
+
+
+
+
+
+For more help on how to use Docker, head to https://docs.docker.com/go/guides/
+PS C:\Users\sanja> docker --version
+Docker version 28.4.0, build d8eb465
+PS C:\Users\sanja>
+
+
+
+
+
+
+
+'
