334e2a30248bfd4538cef31a689b012513065f5a
Ankit Kumar <ankit485803@gmail.com>
2025-10-09 15:22:21 +0530

    rename the month name folder into L1, L2 format  OS lab

---
+++ 303_OS/practical/L4_Oct_9th_Thu/ques_Lab.sh
@@ -0,0 +1,19 @@
+
+
+:'
+
+
+9th Oct 2025 (Thursday)
+
+this lab in 3 parts: 
+1. write function how to do in WSL MNT Linux Ubuntu
+2. create the array
+3.  How to create the tree in Linux bash shell scripting 
+
+
+
+sir uses this online bash scripting compiler in today lab: https://www.tutorialspoint.com/compilers/online-bash-compiler.htm
+
+
+
+'
