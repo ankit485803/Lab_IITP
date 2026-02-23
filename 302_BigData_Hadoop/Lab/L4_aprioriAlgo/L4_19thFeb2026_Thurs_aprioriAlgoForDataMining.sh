8a9fc8793001e90f8e2a061d974f9430bdb4ac05
Ankit Kumar <ankit485803@gmail.com>
2026-02-23 18:47:50 +0530

    Lab4 bigData302 on AprioriAlgo for dataMining using mapReduce

---
+++ 302_BigData_Hadoop/Lab/L4_aprioriAlgo/L4_19thFeb2026_Thurs_aprioriAlgoForDataMining.sh
@@ -0,0 +1,21 @@
+
+
+
+:'
+
+
+19th Feb 2026 (Thursday)  Lab4 on Apriori algo for dataMining
+
+
+The Apriori algorithm is a foundational association rule learning method used to uncover relationships and hidden patterns 
+within large datasets
+
+The Apriori algorithm is an unsupervised machine learning algorithm used for association rule learning. 
+Association rule learning is a data mining technique that identifies frequent patterns, connections and dependencies among different
+ groups of items called itemsets in data.
+
+
+
+
+
+'
