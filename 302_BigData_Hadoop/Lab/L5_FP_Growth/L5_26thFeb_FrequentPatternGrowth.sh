99384d49a7502d017dee2173669134629d8a822f
Ankit Kumar <ankit485803@gmail.com>
2026-02-26 21:10:36 +0530

    Lab5 on FreqPatternGrowth FPG bigData302

---
+++ 302_BigData_Hadoop/Lab/L5_FP_Growth/L5_26thFeb_FrequentPatternGrowth.sh
@@ -0,0 +1,15 @@
+
+
+
+:'
+
+26th Feb 2026 (Thursday) Lab5 on the FP Growth in dataMining
+
+
+
+FP Growth (Frequent Pattern Growth) algorithm is a highly efficient data mining method for discovering frequent itemsets in large datasets without costly candidate generation, unlike the Apriori algorithm. It builds a compressed tree structure (FP-tree) to store data in just two scans. 
+It is commonly used in retail for market basket analysis, such as identifying items often bought together
+
+
+
+'
