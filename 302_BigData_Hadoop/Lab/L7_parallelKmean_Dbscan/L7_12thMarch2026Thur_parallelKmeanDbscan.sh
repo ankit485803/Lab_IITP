dbb5a6dedc05f1e0dcedc178c1ddea85dc9cb720
Ankit Kumar <ankit485803@gmail.com>
2026-03-12 20:32:28 +0530

    today lab7 is based on Parallel KMeans and DBSCAN on bigData

---
+++ 302_BigData_Hadoop/Lab/L7_parallelKmean_Dbscan/L7_12thMarch2026Thur_parallelKmeanDbscan.sh
@@ -0,0 +1,17 @@
+
+
+:'
+
+
+
+12th March 2026 (Thursday)
+
+
+today lab7 is based on Paralle KMeans and DBSCAN on bigData
+
+we will find codeFile on google drive sunaina485803@gmail.com with Lab_IITP folder name
+
+
+
+
+'
