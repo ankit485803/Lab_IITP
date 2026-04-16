91343e8b37257286a1236774a286c4766d37c0b6
Ankit Kumar <ankit485803@gmail.com>
2026-04-16 19:18:35 +0530

    setup the directories for Lab 11 bigData on Introduction to MongoDB Atlas and its integration with Google Colab

---
+++ 302_BigData_Hadoop/Lab/L11_MongoDB/L11_16thApril2026Thurs.sh
@@ -0,0 +1,14 @@
+
+
+:'
+
+16th April 2026 (Thursday)
+
+Lab 11 on MongoDB ATLAS in bigData and code in google colab and next lab will be on atlas software
+
+
+link: 
+mongodb+srv://sr_db:12345@cluster0.wovqgoc.mongodb.net/?appName=Cluster0
+
+
+'
