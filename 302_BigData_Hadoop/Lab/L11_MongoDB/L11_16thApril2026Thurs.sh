b69664d90a5a6310b582ddad4814b29bda2d3165
Ankit Kumar <ankit485803@gmail.com>
2026-04-17 18:09:16 +0530

    added some imp command to main file regardig .env file

--- 302_BigData_Hadoop/Lab/L11_MongoDB/L11_16thApril2026Thurs.sh
+++ 302_BigData_Hadoop/Lab/L11_MongoDB/L11_16thApril2026Thurs.sh
@@ -7,3 +7,14 @@
 Lab 11 on MongoDB ATLAS in bigData and code in google colab and next lab will be on atlas software
 
 
+You now have this in your repo:
+create mode 100644 .../.env
+That means your .env file is currently tracked by Git — which is a security risk if it contains credentials. and jo github pe publicly 
+visible ho sakta hai
+
+
+PS C:\Users\sanja\Desktop\GitProj\Lab_IITP> git rm --cached 302_BigData_Hadoop/Lab/L11_MongoDB/.env
+PS C:\Users\sanja\Desktop\GitProj\Lab_IITP> git commit -m "fix(security): remove .env from tracking"
+
+
+:
