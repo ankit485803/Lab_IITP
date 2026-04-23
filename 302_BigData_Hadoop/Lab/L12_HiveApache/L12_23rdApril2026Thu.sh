1eb515551804f99438229b7b40b74b1b906232a5
Ankit Kumar <ankit485803@gmail.com>
2026-04-23 21:03:51 +0530

    to create the tag for this v2.3-HiveApacheQL

--- 302_BigData_Hadoop/Lab/L12_HiveApache/L12_23rdApril2026Thu.sh
+++ 302_BigData_Hadoop/Lab/L12_HiveApache/L12_23rdApril2026Thu.sh
@@ -12,3 +12,18 @@ Data summarization, reporting, and ETL (extract-transform-load)
 
 
 '
+
+
+git tag -a v2.3-HiveApacheQL -m "23rd April 2026 (Thursday) Lab 12: Big Data using Apache Hive
+
+VVVIMP Points:
+- Apache Hive is a data warehouse tool built on Hadoop
+- Uses HiveQL (SQL-like language)
+- Suitable for batch processing, not real-time
+- Handles large datasets (GB/TB scale)
+- High latency is a major limitation
+- Poor performance with many small files"
+
+
+git push origin v2.3-HiveApacheQL
+git show v2.3-HiveApacheQL
