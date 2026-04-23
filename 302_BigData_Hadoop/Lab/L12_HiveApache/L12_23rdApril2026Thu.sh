044134e4ca76968ab7e4a188e1cfd119f3f6ffc7
Ankit Kumar <ankit485803@gmail.com>
2026-04-23 20:56:13 +0530

    done Lab12 bigData on Hive apache intro session today

---
+++ 302_BigData_Hadoop/Lab/L12_HiveApache/L12_23rdApril2026Thu.sh
@@ -0,0 +1,14 @@
+
+
+:'
+
+23rd April 2026 (Thursday) Lab 12 is based on Hive Apache
+Big data
+
+
+Apache Hive is a data warehouse system built on top of Apache Hadoop. 
+It lets you query large datasets using a SQL-like language called HiveQL instead of writing complex Java code.
+Data summarization, reporting, and ETL (extract-transform-load)
+
+
+'
