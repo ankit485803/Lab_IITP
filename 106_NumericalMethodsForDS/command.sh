1978a3725252c33126ede6fc529e986422b44ced
Ankit Kumar <ankit485803@gmail.com>
2026-04-26 21:36:58 +0530

    added the command.sh files to 106_NumericalMethodsForDS

---
+++ 106_NumericalMethodsForDS/command.sh
@@ -0,0 +1,8 @@
+
+
+git commit `
+-m "Add Numerical Methods for Data Science modules" `
+-m "Includes Nume_Diff (forward/backward difference operators)" `
+-m "Includes Num_Interg (trapezoidal, composite trapezoidal, Simpson 1/3, Gauss Legendre, Hermite quadrature)" `
+-m "Includes Linear_Alg (LU, PLU, QR, Cholesky, DoLittle, Gauss elimination, Gauss-Jordan)" `
+-m "Includes interpolation concepts in 1D, 2D, 3D for data science applications"
